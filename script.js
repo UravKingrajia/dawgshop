ae3e5c629b77990f0ff671a9e199a49d6fb1badd
Urav Hingrajia <uravh15@gmail.com>
2017-06-29 19:15:05 +0000

    Dawg it Up

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-body {
-    background-color: red;
-}
+document.getElementById('go').onclick = function() {
+    var inputText = $('#text-input').val();
+    var dawgifiedText = inputText + " = The Best";
+    document.getElementById('output').innerHTML = dawgifiedText;
+    
+};
+
